517fe0af09a117c670e796d4277b556d7b7e0c7a
Roman Danylyk <romanbp@ukr.net>
2022-05-14 13:33:13 +0300

    правки

--- js/menu.js
+++ js/menu.js
@@ -14,15 +14,5 @@
 
 
     })
-    // refs.openModalBtn.addEventListener("click", toggleModal);
-    // refs.closeModalBtn.addEventListener("click", toggleModal);
 
-    // const refs = {
-    //     openModalBtn: document.querySelector("[data-modal-open]"),
-    //     closeModalBtn: document.querySelector("[data-modal-close]"),
-    //     modal: document.querySelector("[data-modal]"),
-    // };
-    // function toggleModal() {
-    //     refs.modal.classList.toggle("is-hidden");
-    // }
 })();
